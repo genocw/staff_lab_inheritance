5f232e394659099f0d0cea0fb22b7e435437193c
Genna Walsh <gennawalsh@gmail.com>
2018-11-26 16:16:52 +0000

    add abstract employee class

---
+++ src/main/java/staff/Employee.java
@@ -0,0 +1,36 @@
+package staff;
+
+public abstract class Employee {
+
+    private String name;
+    private String numberNI;
+    private double salary;
+
+    public Employee(String name, String numberNI, double salary) {
+        this.name = name;
+        this.numberNI = numberNI;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getNumberNI() {
+        return this.numberNI;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void raiseSalary(double addAmount) {
+        if (addAmount > 0) {
+            this.salary += addAmount;
+        }
+    }
+
+    public double payBonus() {
+        return (this.salary / 100);
+    }
+}
